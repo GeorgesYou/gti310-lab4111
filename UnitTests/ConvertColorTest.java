5c61bc7bc67cb28adf8c9930ca8919819b5205b9
tari.calaelen20 <tari.calaelen20@gmail.com>
2010-11-16 16:22:56 +0000

    Ajout classe convertColor et convertColorTest.

---
+++ UnitTests/ConvertColorTest.java
@@ -0,0 +1,12 @@
+package UnitTests;
+
+import gti310.tp4.Main;
+import junit.framework.TestCase;
+
+public class ConvertColorTest extends TestCase {
+
+	public void testConvertRGBToYUV(){
+		
+
+	}
+}
