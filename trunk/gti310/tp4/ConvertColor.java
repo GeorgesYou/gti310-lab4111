0f729fcb67c83ba3499eb83698c6f7f9ce6e547c
tari.calaelen20@gmail.com <tari.calaelen20@gmail.com@5d5da981-23df-6f4f-f3eb-b148c1528d93>
2010-11-23 15:24:34 +0000

    Rectification si valeur  YUV < 0.

--- trunk/gti310/tp4/ConvertColor.java
+++ trunk/gti310/tp4/ConvertColor.java
@@ -1,27 +1,67 @@
 package gti310.tp4;
 import java.util.*;
+import java.math.*;
 
 public class ConvertColor {
 
 	// Matrice de conversion, source: https://cours.etsmtl.ca/gti310/private/Presentations/GTI310-3-ConceptsFondamentauxVideo-v14.pdf
 	//private double matrice[][] = {{0.299,0.587,0.114},{-0.147,-0.289,0.436},{0.615, -0.515, -0.100}};
 	
-	public double[][][] convertRGBToYUV(int[][][] image)
+	public int[][][] convertRGBToYUV(int[][][] image)
 	{
 		
-		double[][][] imageYUV = new double[Main.COLOR_SPACE_SIZE][][];
+		int[][][] imageYUV = new int[Main.COLOR_SPACE_SIZE][][];
 		
 		for(int i = 0; i < image.length; i++)
 		{
 			for(int j = 0; j < image[0].length;j++)
 			{
-				double Y = 0.299*image[Main.R][i][j] + 0.587*image[Main.G][i][j] +0.114*image[Main.B][i][j];
-				double U = 0.492*(image[Main.B][i][j]-Y);
-				double V = 0.877*(image[Main.R][i][j]-Y);
+							
+				int Y = (int)(0.299*image[Main.R][i][j] + 0.587*image[Main.G][i][j] +0.114*image[Main.B][i][j]);
+				int U = (int)(0.492*(image[Main.B][i][j]-Y));
+				int V = (int)(0.877*(image[Main.R][i][j]-Y));
 				
-				imageYUV[Main.Y][i][j]=Y;
+
+				
+				if(Y < 0 )
+				{
+					System.out.println("Couleur négative Y : " + Y);
+					Y = (Math.abs(Y)*255)/(255+Math.abs(Y));
+					
+					System.out.println("Couleur corrigée Y : " +Y);
+				}else if(Y>255)
+				{
+					
+				}
+				
+				if(U < 0)
+				{
+					System.out.println("Couleur négative U : " +U);
+					
+					U = (Math.abs(U)*255)/(255+Math.abs(U));
+						
+					System.out.println("Couleur corrigée U : " +U);
+				}
+				else if(U>255)
+				{
+					
+				}
+				
+				if(V < 0 )
+				{
+					System.out.println("Couleur négative V : " +V);
+					V = (Math.abs(V)*255)/(255+Math.abs(V));	
+					System.out.println("Couleur corrigée V : " +V);
+				}else if(V>255)
+				{
+					
+				}
+				
+				
+			/*	imageYUV[Main.Y][i][j]=Y;
 				imageYUV[Main.U][i][j]=U;
-				imageYUV[Main.V][i][j]=V;				
+				imageYUV[Main.V][i][j]=V;	
+				*/
 			}			
 		}
 		
