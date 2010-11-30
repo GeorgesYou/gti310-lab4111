1f05e95aff528615da2658b4dcd8083849b9c819
tari.calaelen20@gmail.com <tari.calaelen20@gmail.com@5d5da981-23df-6f4f-f3eb-b148c1528d93>
2010-11-30 16:04:08 +0000

    ConvertColor et ConvertColorTest sont finis.

--- trunk/UnitTests/ConvertColorTest.java
+++ trunk/UnitTests/ConvertColorTest.java
@@ -1,12 +1,68 @@
 package UnitTests;
 
+import gti310.tp4.BlocManager;
+import gti310.tp4.ConvertColor;
 import gti310.tp4.Main;
 import junit.framework.TestCase;
 
 public class ConvertColorTest extends TestCase {
 
-	public void testConvertRGBToYUV(){
+	public void testConvertYUVToRGB(){
 		
+		int[][][] imageRGB = new int[Main.COLOR_SPACE_SIZE][256][256];
+					
+		for(int i = 0; i <256 ; i++)
+		{
+			for(int j = 0; j < 256 ; j++)
+			{
+				imageRGB[Main.R][i][j] = j;
+				imageRGB[Main.G][i][j] = j;
+				imageRGB[Main.B][i][j] = j;
+			}
+		}
+	
+		int[][][] imageYUV = ConvertColor.convertRGBToYUV(imageRGB);
 		
+		int[][][] imageRGB2 = ConvertColor.convertYUVToRGB(imageYUV);
+		
+		for(int i = 0; i <256 ; i++)
+		{
+			for(int j = 0; j < 256 ; j++)
+			{
+				assertTrue((imageRGB2[Main.R][i][j]>= 0) && imageRGB2[Main.R][i][j]<256);
+				assertTrue((imageRGB2[Main.G][i][j]>=0) && imageRGB2[Main.R][i][j]<256);
+				assertTrue((imageRGB2[Main.B][i][j]>=0) && imageRGB2[Main.R][i][j]<256);
+			}
+		}
+		
+	}
+	
+	public void testConvertYUVToRGB2(){
+		
+		int[][][] imageRGB = new int[Main.COLOR_SPACE_SIZE][256][256];
+		
+		for(int i = 0; i <256 ; i++)
+		{
+			for(int j = 0; j < 256 ; j++)
+			{
+				imageRGB[Main.R][i][j] = j;
+				imageRGB[Main.G][i][j] = j;
+				imageRGB[Main.B][i][j] = j;
+			}
+		}
+	
+		int[][][] imageYUV = ConvertColor.convertRGBToYUV(imageRGB);
+		
+		int[][][] imageRGB2 = ConvertColor.convertYUVToRGB(imageYUV);
+		
+		for(int i = 0; i <256 ; i++)
+		{
+			for(int j = 0; j < 256 ; j++)
+			{
+				assertTrue(imageRGB2[Main.R][i][j]-imageRGB[Main.R][i][j] <=1);
+				assertTrue(imageRGB2[Main.G][i][j]-imageRGB[Main.G][i][j]<=1);
+				assertTrue(imageRGB2[Main.B][i][j]-imageRGB[Main.B][i][j]<=1);
+			}
+		}
 	}
 }
--- trunk/gti310/tp4/ConvertColor.java
+++ trunk/gti310/tp4/ConvertColor.java
@@ -7,14 +7,14 @@ public class ConvertColor {
 	// Matrice de conversion, source: https://cours.etsmtl.ca/gti310/private/Presentations/GTI310-3-ConceptsFondamentauxVideo-v14.pdf
 	//private double matrice[][] = {{0.299,0.587,0.114},{-0.147,-0.289,0.436},{0.615, -0.515, -0.100}};
 	
-	public int[][][] convertRGBToYUV(int[][][] image)
+	public static int[][][] convertRGBToYUV(int[][][] image)
 	{
 		
-		int[][][] imageYUV = new int[Main.COLOR_SPACE_SIZE][image.length][image[0].length];
+		int[][][] imageYUV = new int[Main.COLOR_SPACE_SIZE][image[0].length][image[0][0].length];
 		
-		for(int i = 0; i < image.length; i++)
+		for(int i = 0; i < image[0].length; i++)
 		{
-			for(int j = 0; j < image[0].length;j++)
+			for(int j = 0; j < image[0][0].length;j++)
 			{
 							
 				int Y = (int)(0.299*image[Main.R][i][j] + 0.587*image[Main.G][i][j] +0.114*image[Main.B][i][j]);
@@ -30,26 +30,29 @@ public class ConvertColor {
 		return imageYUV;
 	}
 	
-	/*public int[][][] convertYUVToRGB(int[][][] image)
+	public static int[][][] convertYUVToRGB(int[][][] image)
 	{
-		int[][][] imageRGB = new int[Main.COLOR_SPACE_SIZE][image.length][image[0].length];
+		int[][][] imageRGB = new int[Main.COLOR_SPACE_SIZE][image[0].length][image[0][0].length];
 		
-		for(int i = 0; i < image.length; i++)
+		for(int i = 0; i < image[0].length; i++)
 		{
-			for(int j = 0; j < image[0].length;j++)
-			{
-							
-				int R = (int)(0.299*image[Main.R][i][j] + 0.587*image[Main.G][i][j] +0.114*image[Main.B][i][j]);
-				int G = (int)(0.492*(image[Main.B][i][j]-Y));
-				int B = (int)(0.877*(image[Main.R][i][j]-Y));
-				
+			for(int j = 0; j < image[0][0].length;j++)
+			{		
+				int R = (int)(1.140250855*(image[Main.V][i][j]+.877*image[Main.Y][i][j]));
+				int G = (int)(-0.3947313749*(image[Main.U][i][j]+1.471403709*(image[Main.V][i][j]-1.721795786*image[Main.Y][i][j])));
+				int B = (int)(2.032520325*(image[Main.U][i][j]+0.492*image[Main.Y][i][j]));
+		
+				R = (R<0 ? 0 :( R > 255 ? 255 : R) );
+				G = (G<0 ? 0 :( G > 255 ? 255 : G) );
+				B = (B<0 ? 0 :( B > 255 ? 255 : B) );				
+		
 				imageRGB[Main.R][i][j]=R;
 				imageRGB[Main.G][i][j]=G;
-				imageRGB[Main.B][i][j]=B;					
+				imageRGB[Main.B][i][j]=B;				
 			}			
 		}	
-		
+
 		return imageRGB;
-	}*/
+	}
 	
 }
